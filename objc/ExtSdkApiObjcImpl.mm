1678fcea5acbfecd57d233f8fe8f84ff2bf388f8
asterisk <asterisk@djpmini.local>
2022-03-14 16:06:58 +0800

    update

--- objc/ExtSdkApiObjcImpl.mm
+++ objc/ExtSdkApiObjcImpl.mm
@@ -1,18 +1,18 @@
 #include "ExtSdkApiObjcImpl.h"
 #include "ExtSdkObjectObjcImpl.h"
-#import "ExtSdkDispatchObjc.h"
+#import "ExtSdkDispatch.h"
 
 
 EXT_SDK_NAMESPACE_BEGIN
 
 void ExtSdkApiObjcImpl::init(const std::shared_ptr<ExtSdkObject> config) {
     std::shared_ptr<ExtSdkObjectObjcImpl> cpp_config = std::dynamic_pointer_cast<ExtSdkObjectObjcImpl>(config);
-    [[ExtSdkDispatchObjc getInstance] init: cpp_config->obj];
+    [[ExtSdkDispatch getInstance] init: cpp_config->obj];
 }
 
 void ExtSdkApiObjcImpl::addListener(const std::shared_ptr<ExtSdkObject> listener) {
     std::shared_ptr<ExtSdkObjectObjcImpl> cpp_listener = std::dynamic_pointer_cast<ExtSdkObjectObjcImpl>(listener);
-    [[ExtSdkDispatchObjc getInstance] addListener:(id<ExtSdkDelegateObjc>)cpp_listener->obj];
+    [[ExtSdkDispatch getInstance] addListener:(id<ExtSdkDelegateObjc>)cpp_listener->obj];
 }
 
 void ExtSdkApiObjcImpl::delListener(const std::shared_ptr<ExtSdkObject> listener) {
@@ -25,11 +25,11 @@ void ExtSdkApiObjcImpl::callSdkApi(const std::string &methodType, const std::sha
     std::shared_ptr<ExtSdkObjectObjcImpl> cpp_callback = std::dynamic_pointer_cast<ExtSdkObjectObjcImpl>(callback);
     id<ExtSdkCallbackObjc> oc_callback = (id<ExtSdkCallbackObjc>)cpp_callback->obj;
     NSString* oc_method_type = [NSString stringWithUTF8String:methodType.c_str()];
-    [[ExtSdkDispatchObjc getInstance] callSdkApi:oc_method_type withParams:oc_params withCallback:oc_callback];
+    [[ExtSdkDispatch getInstance] callSdkApi:oc_method_type withParams:oc_params withCallback:oc_callback];
 }
 
 void ExtSdkApiObjcImpl::unInit() {
-    [[ExtSdkDispatchObjc getInstance] unInit:nil];
+    [[ExtSdkDispatch getInstance] unInit:nil];
 }
 
 EXT_SDK_NAMESPACE_END
